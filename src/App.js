c7d9f107a4ff7a099a9aef128f1037ba2ae0b4ee
Afonso Dias <afonso-dias2011@hotmail.com>
2019-11-30 23:09:30 -0300

    feat: RSA implementation with random primes (max 4 characters in message)

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
 import React from 'react';
 import './App.css';
+import bigInt from 'big-integer'
 
 export class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       message: '',
-      p: {
-        value: '',
-        isPrime: '',
-      },
-      q: {
-        value: '',
-        isPrime: '',
-      },
+      messageChar: '',
+      encryptedMessage: '',
+      decryptedMessage: '',
+      decodedMessage: '',
+      p: '',
+      q: '',
+      e: '',
+      n: '',
+      d: '',
+      isEncrypted: false,
+      totient: '',
     };
   }
 
-  handleChangeMessage = (e) => {
-    this.setState({ message: e.target.value })
+  getMessageCharArray = (str) => {
+    const codes = str
+      .split('')
+      .map(i => i.charCodeAt())
+      .join('');
+
+    this.setState({ messageChar: codes })
   }
 
-  handleChangeP = (e) => {
-    let p = { ...this.state.p }
-    p.value = e.target.value;
-    this.setState({ p })
-  }
-
-  handleChangeQ = (e) => {
-    let q = { ...this.state.q }
-    q.value = e.target.value;
-    this.setState({ q })
+  handleChangeMessage = async (e) => {
+    await this.setState({ message: e.target.value })
+    this.getMessageCharArray(this.state.message)
   }
 
   isPrime = (num) => {
     if (num <= 1) return false;
     if (num === 2) return true;
 
-    // storing the calculated value would be much 
-    // better than calculating in each iteration
     var sqrt = Math.sqrt(num);
 
     for (var i = 2; i <= sqrt; i++)
@@ -47,32 +47,106 @@ export class App extends React.Component {
   }
 
   enableButton = () => {
-    return this.isPrime(this.state.p.value) && this.isPrime(this.state.q.value);
+    return this.state.message !== '';
+  }
+
+  randomPrime = () => {
+    const min = 1
+    const max = 99999999;
+    
+    while (true) {
+      let p = bigInt.randBetween(min, max);
+      if (this.isPrime(p)) {
+        return p;
+      } 
+    }
+  }
+
+  generateKey = async () => {
+    let e
+    let totient;
+    let p;
+    let q;
+    let n;
+    let d;
+
+    do {
+      p = this.randomPrime();
+      q = this.randomPrime();
+      totient = bigInt.lcm(
+        p-1,
+        q-1
+      );
+      e = bigInt.randBetween(1, totient);
+    } while (bigInt.gcd(e, totient).notEquals(1));
+
+    await this.setState({ e })
+    n = p * q
+    d = e.modInv(totient);
+    await this.setState({ n })
+    await this.setState({ d })
+    await this.setState({ p })
+    await this.setState({ q })
+    await this.setState({ totient })
+
+    this.encrypt(this.state.messageChar, this.state.n, this.state.e)
+  }
+
+  encrypt = (encodedMsg, n, e) => {
+    console.log(bigInt(encodedMsg));
+    let encryptedMessage = bigInt(encodedMsg).modPow(e, n);
+    this.setState({ encryptedMessage });
+    this.setState({isEncrypted: 'true'})
+  }
+
+  decrypt = async (encryptedMsg, d, n) => {
+    let decryptedMessage = bigInt(encryptedMsg).modPow(d, n); 
+    await this.setState({decryptedMessage});
+    this.decode(this.state.decryptedMessage);
+  }
+
+  decode = (code) => {
+    const stringified = code.toString();
+    let decodedMessage = '';
+
+    for (let i = 0; i < stringified.length; i += 2) {
+      let num = Number(stringified.substr(i, 2));
+      
+      if (num <= 30) {
+        decodedMessage += String.fromCharCode(Number(stringified.substr(i, 3)));
+        i++;
+      } else {
+        decodedMessage += String.fromCharCode(num);
+      }
+    }
+
+    this.setState({decodedMessage});
   }
 
 
   render() {
+    let {message, p, q, e, d, n, encryptedMessage, messageChar, isEncrypted, decryptedMessage, decodedMessage, totient} = this.state
     return (
       <div>
         <label>
           Mensagem:
-          <input type="text" value={this.state.message} onChange={this.handleChangeMessage} />
+          <input type="text" value={message} onChange={this.handleChangeMessage} />
         </label>
         <br />
-        <label>
-          Primo P:
-          <input type="number" value={this.state.p.value} onChange={this.handleChangeP} />
-        </label>
-        <br />
-        <label>
-          Primo Q:
-          <input type="number" value={this.state.q.value} onChange={this.handleChangeQ} />
-        </label>
-        <br />
-        <h1>{this.state.message}</h1>
-        <h1>{this.state.p.value}</h1>
-        <h1>{this.state.q.value}</h1>
-        <input type="submit" value="Enviar" disabled={!this.enableButton()} />
+        <h1>Message Char:{messageChar}</h1>
+        <h1>p:{p.toString()}</h1>
+        <h1>q:{q.toString()}</h1>
+        <h1>e:{e.toString()}</h1>
+        <h1>totient:{totient.toString()}</h1>
+        <h1>n:{n}</h1>
+        <h1>d:{d.toString()}</h1>
+        <h1>Encrypted Message:{encryptedMessage.toString()}</h1>
+        <input type="submit" value="Generate Key and Encrypt" disabled={!this.enableButton()} onClick={() => this.generateKey()} />
+        <br/>
+        <input type="submit" value="Decrypt" disabled={!isEncrypted} onClick={() => this.decrypt(encryptedMessage, d, n)} />
+        <br/>
+        <h1>Decrypted Message:{decryptedMessage.toString()}</h1>
+        <h1>Decoded Message:{decodedMessage}</h1>
       </div>
     );
   }
